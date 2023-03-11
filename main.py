b9aed82b55800da61a5e3b8efc78b49b1a1e14a6
defskela <118464418+defskela@users.noreply.github.com>
2023-03-11 21:52:58 +0300

    Отвечает
    Использует данные из сообщения и отвечает по полученным данным

--- main.py
+++ main.py
@@ -1,16 +1,70 @@
-# This is a sample Python script.
+from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+BOT_TOKEN = '6013111011:AAF8HwDbWB22mjumWf5i3spHsslf3tnjFh8'
+
+# reply_keyboard = [['/start', '/help'],
+#                   ['/address', '/phone'],
+#                   ['/site', '/work_time']]
+#
+# markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+async def start(update, context):
+    await update.message.reply_text(
+        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
+        "Вы можете прервать опрос, послав команду /stop.\n"
+        "В каком городе вы живёте?")
+    return 1
 
 
-# Press the green button in the gutter to run the script.
+async def first_response(update, context):
+    locality = update.message.text
+    await update.message.reply_text(
+        f"Какая погода в городе {locality}?")
+    # Следующее текстовое сообщение будет обработано
+    # обработчиком states[2]
+    return 2
+
+
+async def second_response(update, context):
+    await update.message.reply_text(
+        f"Любая погода - дар природы!")
+    await update.message.reply_text("Спасибо за участие в опросе! Всего доброго!")
+    return ConversationHandler.END  # Константа, означающая конец диалога.
+    # Все обработчики из states и fallbacks становятся неактивными.
+
+
+async def stop(update, context):
+    await update.message.reply_text("Всего доброго!")
+    return ConversationHandler.END
+
+
+conv_handler = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('start', start)],
+
+        # Состояние внутри диалога.
+        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
+        states={
+            # Функция читает ответ на первый вопрос и задаёт второй.
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
+            # Функция читает ответ на второй вопрос и завершает диалог.
+            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)]
+        },
+
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(conv_handler)
+    application.run_polling()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
