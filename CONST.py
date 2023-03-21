db012545922c969742d033043ec32f237273dc73
defskela <118464418+defskela@users.noreply.github.com>
2023-03-21 19:54:21 +0300

    Дополнена структура бота, добавлено приветствие

--- CONST.py
+++ CONST.py
@@ -1,7 +1,16 @@
 bytic_address = 'Сиреневый бульвар, 11, Троицк, Москва'  # адрес байтика на сиреневом
 geocoder_uri = "http://geocode-maps.yandex.ru/1.x/"  # для запроса статик апи
 MAIN_MENU, MENU_1, MENU_5, QUE_1, QUE_5 = range(5)  # для возврата CommandHandler'а
+MENU_1_1, MENU_1_2 = 11, 12
+GROUPS_1 = 'groups_1_4'
 TOKEN = '6013111011:AAF8HwDbWB22mjumWf5i3spHsslf3tnjFh8'  # токен бота
 
+# расписание нахождения в Байтике для смен
 with open('ordinary.txt', 'r', encoding='utf-8') as ordinar:
     ordinary = ordinar.read()
+
+# описание летних смен (1-й и 2-й)
+with open('description_1.txt', 'r', encoding='utf-8') as desk:
+    desk_1 = desk.read()
+with open('description_2.txt', 'r', encoding='utf-8') as desk:
+    desk_2 = desk.read()
--- for_Bytic.py
+++ for_Bytic.py
@@ -1,6 +1,7 @@
 from db_data import db_session
 from logger import *
 from CONST import *
+
 from geocode import api_request
 from telegram import __version__ as TG_VER
 from telegram import ReplyKeyboardRemove, Update
@@ -12,7 +13,6 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
-
 
 try:
     from telegram import __version_info__
@@ -26,9 +26,9 @@ if __version_info__ < (20, 0, 0, "alpha", 5):
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
     )
 
-db_session.global_init('db_data/questions.db') # инициализация бд
+db_session.global_init('db_data/questions.db')  # инициализация бд
 
-from markups_and_ques import * # после инициализации импортируем все markupы
+from markups_and_ques import *  # после инициализации импортируем все markupы, не переносить наверх
 
 
 # помощь
@@ -54,23 +54,56 @@ async def main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE, back_mes
     user = update.message.from_user
     if '1-4 классы' in [update.message.text, back_message]:
         await update.message.reply_text(
-            "Выберите нужное вам действие",
+            "Выберите интересующее Вас направление",
             reply_markup=markup_1)
         return MENU_1
     elif '5-11 классы' in [update.message.text, back_message]:
         await update.message.reply_text(
-            "Выберите нужное вам действие",
+            "Выберите интересующее Вас направление",
             reply_markup=markup_5)
         return MENU_5
+    else:
+        await update.message.reply_text(
+            "Я Вас не понимаю☹ Пожалуйста, выберите нужный Вам класс")
 
 
 async def menu_1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    if update.message.text == 'Расписание':
+    if update.message.text == 'Летние образовательные смены':
+        await update.message.reply_text('Выберите интересующий Вас вопрос', reply_markup=groups)
+        return GROUPS_1
+    elif update.message.text == 'Летние курсы':
+        await update.message.reply_text('Выберите интересующий Вас курс', reply_markup=markup_1_2)
+        return MENU_1_2
+    elif update.message.text == 'Назад':
+        return await start(update, context, back=True)
+    else:
+        await update.message.reply_text(
+            "Я Вас не понимаю☹ Пожалуйста, выберите интересующее Вас направление")
+
+
+async def groups_1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    if update.message.text == '1-я смена (29 мая - 9 июня)':
+        await update.message.reply_text('Выберите интересующую Вас смену', reply_markup=markup_1_1)
+        return MENU_1_1
+    elif update.message.text == '2-я смена (13 июня - 23 июня)':
+        await update.message.reply_text('Выберите интересующую Вас смену', reply_markup=markup_1_1)
+        return MENU_1_1
+    else:
+        await update.message.reply_text(
+            "Я Вас не понимаю☹ Пожалуйста, выберите интересующую Вас смену")
+
+
+async def menu_1_1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    # нужно разделить на два описания
+    if update.message.text == 'Описание':
+        await update.message.reply_text(
+            f"{desk_1}")
+    elif update.message.text == 'Расписание':
         await update.message.reply_text(
             f"{ordinary}")
     elif update.message.text == 'Контакты':
         await update.message.reply_text(
-            "Телефон: +84958510367")
+            "Телефон: +84959559470\nЭлектронная почта: bytic@bytic.ru")
     elif update.message.text == 'Стоимость':
         await update.message.reply_text(
             "Стоимость - 2500 за день лагеря")
@@ -94,6 +127,23 @@ async def menu_1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
             api_request(response),
             caption=bytic_address
         )
+    else:
+        await update.message.reply_text(
+            "Я Вас не понимаю☹ Пожалуйста, выберите интересующий Вас вопрос")
+
+
+async def menu_1_2(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    if update.message.text == 'Каллиграфия':
+        await update.message.reply_text('Тут будет информация о курсе Каллиграфии')
+    elif update.message.text == 'Курс1':
+        await update.message.reply_text('Тут будет информация о курсе1')
+    elif update.message.text == 'Курс2':
+        await update.message.reply_text('Тут будет информация о курсе2')
+    elif update.message.text == 'Назад':
+        return await start(update, context, back=True)
+    else:
+        await update.message.reply_text(
+            "Я Вас не понимаю☹ Пожалуйста, выберите интересующий Вас курс")
 
 
 async def menu_5(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -103,7 +153,7 @@ async def menu_5(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
             f"{ordinary}")
     elif update.message.text == 'Контакты':
         await update.message.reply_text(
-            "Телефон: +84958510367")
+            "Телефон: +84959559470\nЭлектронная почта: bytic@bytic.ru")
     elif update.message.text == 'Стоимость':
         await update.message.reply_text(
             "Стоимость - 2500 за день лагеря")
@@ -127,6 +177,9 @@ async def menu_5(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
             api_request(response),
             caption=bytic_address
         )
+    else:
+        await update.message.reply_text(
+            "Я Вас не понимаю☹ Пожалуйста, выберите интересующий Вас вопрос")
 
 
 async def que_1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -137,6 +190,9 @@ async def que_1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
                 reply_markup=markup_q_1)
     if update.message.text == 'Назад':
         return await main_menu(update, context, back_message='1-4 классы')
+    else:
+        await update.message.reply_text(
+            "Данного вопроса нет в списках☹ Пожалуйста, выберите интересующий Вас вопрос")
 
 
 async def que_5(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -147,26 +203,9 @@ async def que_5(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
                 reply_markup=markup_q_5)
     if update.message.text == 'Назад':
         return await main_menu(update, context, back_message='5-11 классы')
-
-
-# async def skip_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-#     """Skips the location and asks for info about the user."""
-#     user = update.message.from_user
-#     logger.info("User %s did not send a location.", user.first_name)
-#     await update.message.reply_text(
-#         "You seem a bit paranoid! At last, tell me something about yourself."
-#     )
-#
-#     return BIO
-
-
-# async def bio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-#     """Stores the info about the user and ends the conversation."""
-#     user = update.message.from_user
-#     logger.info("Bio of %s: %s", user.first_name, update.message.text)
-#     await update.message.reply_text("Thank you! I hope we can talk again some day.")
-#
-#     return ConversationHandler.END
+    else:
+        await update.message.reply_text(
+            "Данного вопроса нет в списках☹ Пожалуйста, выберите интересующий Вас вопрос")
 
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -189,11 +228,14 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
-            MAIN_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, main_menu)],
-            MENU_1: [MessageHandler(filters.TEXT & ~filters.COMMAND, menu_1)],
-            MENU_5: [MessageHandler(filters.TEXT & ~filters.COMMAND, menu_5)],
-            QUE_1: [MessageHandler(filters.TEXT & ~filters.COMMAND, que_1)],
-            QUE_5: [MessageHandler(filters.TEXT & ~filters.COMMAND, que_5)],
+            MAIN_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, main_menu)],  # главное меню
+            MENU_1: [MessageHandler(filters.TEXT & ~filters.COMMAND, menu_1)],  # выбор для 1-4 класса
+            GROUPS_1: [MessageHandler(filters.TEXT & ~filters.COMMAND, groups_1)],
+            MENU_1_1: [MessageHandler(filters.TEXT & ~filters.COMMAND, menu_1_1)],  # летние программы
+            MENU_1_2: [MessageHandler(filters.TEXT & ~filters.COMMAND, menu_1_2)],  # летние курсы
+            MENU_5: [MessageHandler(filters.TEXT & ~filters.COMMAND, menu_5)],  # летние программы 5-11
+            QUE_1: [MessageHandler(filters.TEXT & ~filters.COMMAND, que_1)],  # вопросы 1-4
+            QUE_5: [MessageHandler(filters.TEXT & ~filters.COMMAND, que_5)],  # вопросы 5-11
         },
         fallbacks=[CommandHandler("cancel", cancel)],
         allow_reentry=True
