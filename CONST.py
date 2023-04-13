e60d35a301e5972e5fd46a3a3640377e5ef75166
defskela <118464418+defskela@users.noreply.github.com>
2023-04-13 15:52:35 +0300

    Проверка корректности всех файлов

--- CONST.py
+++ CONST.py
@@ -1,4 +1,23 @@
-bytic_address = 'Сиреневый бульвар, 11, Троицк, Москва'  # адрес байтика на сиреневом
+bytic_address_1 = 'Сиреневый бульвар, 11, Троицк, Москва'  # адрес байтика на сиреневом
+bytic_address_2 = 'микрорайон В, 39, Троицк, Москва'  # адрес байтика на сиреневом
 geocoder_uri = "http://geocode-maps.yandex.ru/1.x/"  # для запроса статик апи
+
+
 MAIN_MENU, MENU_1, MENU_5, QUE_1, QUE_5 = range(5)  # для возврата CommandHandler'а
-TOKEN = '6013111011:AAF8HwDbWB22mjumWf5i3spHsslf3tnjFh8'  # токен бота
+MENU_1_1, MENU_1_2 = 11, 12  # для вызова ф-ций menu_1_1 и menu_1_2
+GROUPS_1 = 'groups_1_4'  # для вызова ф-ции groups_1
+COURSES = 121  # для ф-ции курсов 1-4 классов
+ADDRESS = 'address'
+
+TOKEN = '5846392119:AAHq6hs73GVd6UOPw6rQksc18y2QhRK2_Qw'  # токен бота
+
+
+# расписание нахождения в Байтике для смен
+with open('ordinary.txt', 'r', encoding='utf-8') as ordinar:
+    ordinary = ordinar.read()
+
+# описание летних смен (1-й и 2-й)
+with open('description_1.txt', 'r', encoding='utf-8') as desk:
+    desk_1 = desk.read()
+# with open('description_2.txt', 'r', encoding='utf-8') as desk:
+#     desk_2 = desk.read()
--- markups_and_ques.py
+++ markups_and_ques.py
@@ -1,22 +1,46 @@
 from telegram import ReplyKeyboardMarkup
-from work_with_db import *
+from db_data.work_with_db import get_questions
 
 main_markup = ReplyKeyboardMarkup(
     [["1-4 классы", "5-11 классы"]], one_time_keyboard=False
 )
-markup_1 = ReplyKeyboardMarkup(
-    [['Помощь', 'Телефон'],
-     ['Вопросы', 'Назад'],
-     ['Адрес']], one_time_keyboard=False
-)
-markup_5 = ReplyKeyboardMarkup(
-    [['Помощь1', 'Телефон1'],
-     ['Вопросы1', 'Назад1'],
-     ['Адрес1']], one_time_keyboard=False)
 
-que_dct_1 = que_dct_5 = {q1[0][1]: q1[0][2], q2[0][1]: q2[0][2], q3[0][1]: q3[0][2], q4[0][1]: q4[0][2],
-                         q5[0][1]: q5[0][2], q6[0][1]: q6[0][2], q7[0][1]: q7[0][2], q8[0][1]: q8[0][2],
-                         q9[0][1]: q9[0][2], q10[0][1]: q10[0][2], q11[0][1]: q11[0][2], q12[0][1]: q12[0][2],
-                         q13[0][1]: q13[0][2], q14[0][1]: q14[0][2], q15[0][1]: q15[0][2]}
-markup_q_1 = markup_q_5 = ReplyKeyboardMarkup(list(map(lambda x: [x], que_dct_1)) + [['Назад']],
+# предлагает выбрать смену
+groups = ReplyKeyboardMarkup([['1-я смена (29 мая - 9 июня)'],
+                              ['2-я смена (13 июня - 23 июня)']], one_time_keyboard=False)
+
+# меню с выбором действий для 1-4 классов
+markup_1 = ReplyKeyboardMarkup(
+    [['Летние образовательные смены'],
+     ['Летние курсы'],
+     ['Назад']], one_time_keyboard=False)
+
+# меню вопросов для летних образовательных программ для 1-4 классов
+markup_1_1 = ReplyKeyboardMarkup(
+    [['Описание', 'Стоимость', 'Расписание'],
+     ['Вопросы', 'Адрес', 'Контакты'],
+     ['Назад']], one_time_keyboard=False)
+
+# меню вопросов для летних курсов
+markup_1_2 = ReplyKeyboardMarkup(
+    [['Каллиграфия', 'Курс1', 'Курс2'],
+     ['Назад']], one_time_keyboard=False)
+
+# пока что просто вопросы для 5-11 классов
+markup_5 = ReplyKeyboardMarkup(
+    [['Летние курсы'],
+     ['Назад']], one_time_keyboard=False)
+
+markup_courses = ReplyKeyboardMarkup(
+    [['Описание'],
+     ['Записаться на курс'],
+     ['Назад']], one_time_keyboard=False)
+
+markup_address = ReplyKeyboardMarkup(
+    [['Сиреневый бульвар'],
+     ['Назад']], one_time_keyboard=False)
+# ['Микрорайон В'],
+
+que_dct_1 = que_dct_5 = get_questions()  # получение данных из бд
+markup_q_1 = markup_q_5 = ReplyKeyboardMarkup([['Назад']] + list(map(lambda x: [x], que_dct_1)),
                                               one_time_keyboard=False)
