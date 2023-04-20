f0b9412557536e6449274277951e77fa56125d4d
defskela <118464418+defskela@users.noreply.github.com>
2023-04-20 19:15:20 +0300

    Delete remove_from_db.py

--- remove_from_db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect(r'db_data\questions.db')
-cur = conn.cursor()
-result = cur.execute("DELETE FROM questions WHERE id = 16").fetchall()
-conn.commit()
-conn.close()
